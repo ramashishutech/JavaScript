a6f14df0697f703d018d67fba23a5abb0b0c07ec
ramashishutech <ramashishutech@gmail.com>
2023-07-10 14:56:47 +0530

    find the missing number from to arrays

--- 1EvenNumber/app.js
+++ 1EvenNumber/app.js
@@ -1,7 +1,10 @@
 //print the evennumber name 1to10
-let x='';
+
 for(i=0;i<=10;i++){
-    if(i%2==0){
-        console .log ('print the even number',i);
+    //console.log(i);//this console print the i value 0,1,2,3,4,5,6,7,8,9,10
+    if(i%2==0){//this condition check if i devide by two and reminder equil to zero
+        console .log('print even number',i);//this console print 0,2,4,6,8,10,
     }
-}
+ 
+    
+ }
